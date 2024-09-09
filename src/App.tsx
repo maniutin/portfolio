215505608b7a4bb3f44567275cc818b4fcd1cff0
Nick <maniutin@gmail.com>
2024-09-09 13:25:04 -0400

    add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import Main from "./components/Main/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <main className="App">
+      <Main />
+    </main>
   );
 }
 
